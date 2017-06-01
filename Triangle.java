7941bef6ac359d58954b5257b30a47b7e814a4e6
Raven-Lee <devil.li.53@gmail.com>
2017-06-02 00:12:52 +0800

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,20 +1,36 @@
 /**
  * Created by Administrator on 2017/6/1.
- * Creating a  isosceles triangle with number
+ * Creating a isosceles triangle with number
  */
 
 public class Triangle {
 
-    public void initializeTriangle() {
+    public void isoscelesTriangle() {
         for (int i = 1; i < 10; i++) {
             for (int j = 9; j > i; j--) {
-                System.out.print(" ");
+                System.out.print(" ");//for empty space in front of number which make
             }
             for (int k = i; k >= 1; k--) {
-                System.out.print(k);
+                System.out.print(k);//numbers before 1
             }
             for (int l = 2; l <= i; l++) {
-                System.out.print(l);
+                System.out.print(l);//numbers after 1
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+
+    public void asteriskTriangle() {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 9; j > i; j--) {
+                System.out.print(" ");//for empty space in front of number which make
+            }
+            for (int k = i; k >= 1; k--) {
+                System.out.print("*");//numbers before 1
+            }
+            for (int l = 2; l <= i; l++) {
+                System.out.print("*");//numbers after 1
             }
             System.out.println("");
         }
